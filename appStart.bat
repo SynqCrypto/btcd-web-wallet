76085d0e3a04ab06154622a89a7cee72b6be826e
onsightit <steve@onsightit.com>
2015-06-29 09:10:52 -0600

    Start wallet changes

--- appStart.bat
+++ appStart.bat
@@ -1,3 +1,8 @@
+@echo off
+REM Start the btcd node and launch a web browser
+echo Starting BitcoinDark Web Wallet...
 start /min supervisor app.js
-start "" public/index.html
+timeout 5 > NUL
+echo Launching Wallet...
+start "" http://127.0.0.1:8080/
 exit
